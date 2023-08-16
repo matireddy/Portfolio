476d9e9a3fc1c63ad69f6657ee9df0178b734625
krishna0556 <matireddysaikrishna@gmail.com>
2023-08-16 06:46:21 +0530

    replaced home content

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Navbar />
       <Themes />
       <Routes>
-        <Route path="/home" index element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
